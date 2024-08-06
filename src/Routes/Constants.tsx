a6a46f2d62145ab11c068612651de833b7594df8
Sachin <sachin@coneon.in>
2024-08-06 15:58:35 +0530

    sidebar fixed

--- src/Routes/Constants.tsx
+++ src/Routes/Constants.tsx
@@ -1,6 +1,7 @@
+export const Login = "/";
 export const Dashboard = "/dashboard/*";
 export const Home = "/dashboard/";
+export const Users = "/dashboard/users";
 export const Products = "/dashboard/products";
 export const Vendors = "/dashboard/vendors";
-export const Users = "/dashboard/users";
-export const Login = "/";
+export const Orders = "/dashboard/orders";
