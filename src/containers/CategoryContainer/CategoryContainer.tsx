dfc13f7548cf2331127fb6711cfcb18fe64a88e6
Sachin <sachin@coneon.in>
2024-08-07 16:15:08 +0530

    switch functonality added

--- src/containers/CategoryContainer/CategoryContainer.tsx
+++ src/containers/CategoryContainer/CategoryContainer.tsx
@@ -1,8 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 import CategoryPage from "../../components/CategoryPage/CategoryPage";
 
 const CategoryContainer: React.FC = () => {
-  return <CategoryPage />;
+  const [isActive, setIsActive] = useState<boolean>(false);
+
+  const handleActive = () => {
+    setIsActive(!isActive);
+  };
+
+  const labels: string[] = [
+    "S No.",
+    "Category Name",
+    "Status",
+    "Login Type",
+    "Signup Date",
+    "Last Login",
+  ];
+
+  return (
+    <CategoryPage
+      isActive={isActive}
+      handleActive={handleActive}
+      labels={labels}
+    />
+  );
 };
 
 export default CategoryContainer;
